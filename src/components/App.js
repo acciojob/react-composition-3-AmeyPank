9e716267ecd2e04d0c0459f2109fa97a5591568e
AmeyPank <pankantiameya@gmail.com>
2023-05-19 02:30:09 +0530

    two tooltips

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Tooltip from "./Tooltip";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <Tooltip text="This is a tooltip">
+      <span>Hover over me!</span>
+      <div>
+      <span>Another Tooltip!</span>
+      </div>
+      
+    </Tooltip>
+  </div>
+);
 }
 
 export default App
