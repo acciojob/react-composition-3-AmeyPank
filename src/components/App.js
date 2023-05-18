09b8a81749742ab52bc42a4f60d41f9e2faf564b
AmeyPank <pankantiameya@gmail.com>
2023-05-19 03:07:33 +0530

    <h2 className="tooltip">Hover over me</h2>

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div>
       <Tooltip text="This is a tooltip">
-        <h1 className="tooltip">Hover over me</h1>
+        <h2 className="tooltip">Hover over me</h2>
       </Tooltip>
       <Tooltip text="This is another tooltip">
         <p className="tooltip">Hover me to see another tooltip</p>
