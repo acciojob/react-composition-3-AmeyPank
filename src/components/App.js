9835f48041f24d372a213a162461c26628a5340e
AmeyPank <pankantiameya@gmail.com>
2023-05-23 01:47:21 +0530

    please check it now

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Tooltip from './Tooltip';
-import  "../styles/App.css"
+import "../styles/App.css"
 
 
 const App = () => {
   return (
     <div>
       <Tooltip text="This is a tooltip">
-        <h2 className="tooltip">This is a tooltip</h2>
+        <h2>
+          <div className="tooltip">This is a tooltip
+          </div>
+        </h2>
       </Tooltip>
       <Tooltip text="This is another tooltip">
-        <p className="tooltip">This is another tooltip</p>
+        <p >
+          <div className="tooltip">
+            This is another tooltip
+          </div>
+        </p>
       </Tooltip>
     </div>
   );
