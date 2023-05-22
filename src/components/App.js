20e5c6940d38f747bdcf264afef918846f0816df
AmeyPank <pankantiameya@gmail.com>
2023-05-23 01:52:38 +0530

    <Tooltip text="This is another tooltip">         <p>           <div className="tooltip">             This is another tooltip           </div>         </p>       </Tooltip>

--- src/components/App.js
+++ src/components/App.js
@@ -6,19 +6,25 @@ import "../styles/App.css"
 const App = () => {
   return (
     <div>
+      <div>
+
       <Tooltip text="This is a tooltip">
         <h2>
           <div className="tooltip">This is a tooltip
           </div>
         </h2>
       </Tooltip>
+      </div>
+      <div>
       <Tooltip text="This is another tooltip">
-        <p >
+        <p>
           <div className="tooltip">
             This is another tooltip
           </div>
         </p>
       </Tooltip>
+      </div>
+      
     </div>
   );
 };
